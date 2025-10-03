6c694bc4ef1f1465bedee8b63182edc5a5f62d75
abdullah mohamed <abdullahmohamed1122002.com>
2025-10-03 19:32:39 +0300

    edited enviromental health

--- lib/features/enviromental health/views/enviromental_health.dart
+++ lib/features/enviromental health/views/enviromental_health.dart
@@ -29,177 +29,205 @@ class EnviromentalHealth extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // ----- Chart -----
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: LineChart(
-                LineChartData(
-                  backgroundColor: const Color(0xFF2A2D3A),
-                  gridData: FlGridData(show: true, drawVerticalLine: false),
-                  titlesData: FlTitlesData(
-                    leftTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 40,
-                        getTitlesWidget: (value, meta) {
-                          return Text(
-                            value.toInt().toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ), // ✅ أبيض
-                          );
-                        },
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.linear_scale_sharp,
+                  color: AppColors.lightGreen,
+                ),
+
+                title: Text(
+                  "Air Quality Index",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.linear_scale_sharp,
+                  color: AppColors.orange,
+                ),
+
+                title: Text(
+                  "Temperature c",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: LineChart(
+                  LineChartData(
+                    backgroundColor: const Color(0xFF2A2D3A),
+                    gridData: FlGridData(show: true, drawVerticalLine: false),
+                    titlesData: FlTitlesData(
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 40,
+                          getTitlesWidget: (value, meta) {
+                            return Text(
+                              value.toInt().toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ), // ✅ أبيض
+                            );
+                          },
+                        ),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 40,
+                          getTitlesWidget: (value, meta) {
+                            return Text(
+                              value.toInt().toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ), // ✅ أبيض
+                            );
+                          },
+                        ),
+                      ),
+                      rightTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      topTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
                       ),
                     ),
-                    bottomTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 40,
-                        getTitlesWidget: (value, meta) {
-                          return Text(
-                            value.toInt().toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ), // ✅ أبيض
-                          );
-                        },
+                    borderData: FlBorderData(show: false),
+                    minX: 0,
+                    maxX: (aqi.length - 1).toDouble(),
+                    minY: 0,
+                    maxY:
+                        [...aqi, ...temp].reduce((a, b) => a > b ? a : b) +
+                        20, // أكبر قيمة + 20
+                    lineBarsData: [
+                      // AQI Line
+                      LineChartBarData(
+                        spots:
+                            aqi
+                                .asMap()
+                                .entries
+                                .map((e) => FlSpot(e.key.toDouble(), e.value))
+                                .toList(),
+                        isCurved: false,
+                        color: Colors.greenAccent,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true),
                       ),
-                    ),
-                    rightTitles: AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    topTitles: AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
+                      // Temp Line
+                      LineChartBarData(
+                        spots:
+                            temp
+                                .asMap()
+                                .entries
+                                .map((e) => FlSpot(e.key.toDouble(), e.value))
+                                .toList(),
+                        isCurved: false,
+                        color: Colors.orangeAccent,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true),
+                      ),
+                    ],
                   ),
-                  borderData: FlBorderData(show: false),
-                  minX: 0,
-                  maxX: (aqi.length - 1).toDouble(),
-                  minY: 0,
-                  maxY:
-                      [...aqi, ...temp].reduce((a, b) => a > b ? a : b) +
-                      20, // أكبر قيمة + 20
-                  lineBarsData: [
-                    // AQI Line
-                    LineChartBarData(
-                      spots:
-                          aqi
-                              .asMap()
-                              .entries
-                              .map((e) => FlSpot(e.key.toDouble(), e.value))
-                              .toList(),
-                      isCurved: false,
-                      color: Colors.greenAccent,
-                      barWidth: 3,
-                      dotData: FlDotData(show: true),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // ----- Values Row -----
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildCircleIndicator(
+                    "AQI",
+                    aqi.last.toDouble(),
+                    "Good",
+                    Colors.greenAccent,
+                  ),
+                  _buildCircleIndicator(
+                    "Temp",
+                    temp.last.toDouble(),
+                    "",
+                    Colors.orangeAccent,
+                  ),
+                  _buildCircleIndicator(
+                    "Noise",
+                 countryDataModel!.environment.noiseLevel.toDouble(),
+                    "Calm",
+                    Colors.blueAccent,
+                  ),
+                ],
+              ),
+
+              // const SizedBox(height: 10),
+
+              // ----- Data Sources -----
+              // Align(
+              //   alignment: Alignment.centerLeft,
+              //   child: Text(
+              //     "Data Sources",
+              //     style: TextStyle(
+              //       color: Colors.white,
+              //       fontWeight: FontWeight.bold,
+              //     ),
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              // IntrinsicHeight(
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //     children: [
+              //       DataSourceWidget(
+              //         title: "Air Quality:",
+              //         subTitle: "Open Aq api",
+              //       ),
+              //       DataSourceWidget(
+              //         title: "heat Index",
+              //         subTitle: "NASA Landset 9",
+              //       ),
+              //       DataSourceWidget(
+              //         title: "Noise Level",
+              //         subTitle: "Local iot sensors",
+              //       ),
+              //     ],
+              //   ),
+              // ),
+
+              const SizedBox(height: 10),
+
+              // ----- Tips -----
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: AppColors.lightBlack,
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Tips & Actions",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                    // Temp Line
-                    LineChartBarData(
-                      spots:
-                          temp
-                              .asMap()
-                              .entries
-                              .map((e) => FlSpot(e.key.toDouble(), e.value))
-                              .toList(),
-                      isCurved: false,
-                      color: Colors.orangeAccent,
-                      barWidth: 3,
-                      dotData: FlDotData(show: true),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "- Avoid strenuous outdoor activity.\n- Keep windows closed on high pollution days.",
+                      style: TextStyle(color: Colors.white70),
                     ),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-
-            // ----- Values Row -----
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildCircleIndicator(
-                  "AQI",
-                  aqi.last.toDouble(),
-                  "Good",
-                  Colors.greenAccent,
-                ),
-                _buildCircleIndicator(
-                  "Temp",
-                  temp.last.toDouble(),
-                  "",
-                  Colors.orangeAccent,
-                ),
-                _buildCircleIndicator("Noise", 45.0, "Calm", Colors.blueAccent),
-              ],
-            ),
-
-            const SizedBox(height: 20),
-
-            // ----- Data Sources -----
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Data Sources",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            IntrinsicHeight(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  DataSourceWidget(
-                    title: "Air Quality:",
-                    subTitle: "Open Aq api",
-                  ),
-                  DataSourceWidget(
-                    title: "heat Index",
-                    subTitle: "NASA Landset 9",
-                  ),
-                  DataSourceWidget(
-                    title: "Noise Level",
-                    subTitle: "Local iot sensors",
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // ----- Tips -----
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: AppColors.lightBlack,
-              ),
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Tips & Actions",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "- Avoid strenuous outdoor activity.\n- Keep windows closed on high pollution days.",
-                    style: TextStyle(color: Colors.white70),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -211,23 +239,29 @@ class EnviromentalHealth extends StatelessWidget {
     String subtitle,
     Color color,
   ) {
-    return CircularPercentIndicator(
-      animationDuration: 1000,
-      radius: 30,
-      percent: value / 100,
-      lineWidth: 3.0,
-      animation: true,
-      circularStrokeCap: CircularStrokeCap.round,
-      center: Text(
-        '${(value).toInt()}%',
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-          color: AppColors.white,
+    return Column(
+      children: [
+        CircularPercentIndicator(
+          animationDuration: 1000,
+          radius: 30,
+          percent: value / 100,
+          lineWidth: 3.0,
+          animation: true,
+          circularStrokeCap: CircularStrokeCap.round,
+          center: Text(
+            '${(value).toInt()}%',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.white,
+            ),
+          ),
+          progressColor: color,
+          backgroundColor: Colors.white24,
         ),
-      ),
-      progressColor: color,
-      backgroundColor: Colors.white24,
+        SizedBox(height: 5),
+        Text(title, style: TextStyle(color: Colors.white, fontSize: 14)),
+      ],
     );
   }
 }
@@ -243,11 +277,8 @@ class DataSourceWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-    
-    Expanded(
+    return Expanded(
       child: Container(
-           
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: AppColors.lightBlack,
@@ -271,4 +302,3 @@ class DataSourceWidget extends StatelessWidget {
     );
   }
 }
-
